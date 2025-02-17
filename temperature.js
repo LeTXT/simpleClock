7f234b3e8a6b9b8e130577b208eb365c6e583540
Leandro Gomes de Oliveira <LeTXT>
2025-02-17 14:43:18 -0300

    add user current position

--- temperature.js
+++ temperature.js
@@ -1,28 +1,36 @@
 let degrees = document.querySelector('#degrees')
 
-
+let userLocation = null
 const APIkey = '5010c5460e3e9df6f634b1198ce7b362'
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Taboão da Serra&appid=${APIkey}&units=metric`
 
-// fetch(url).then(response => {
-//     if(!response.ok) {
-//         throw new Error('Error')
-//     }
-//     return response.json
-// }).then(data => {
-//     degrees.innerHTML = data.main.temp
-// })
+const currentPosition = (position) => {
+  userLocation = {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude
+  }
 
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error');
-    }
-    return response.json();
-  })
-  .then(data => {
-    degrees.innerHTML = `${parseInt(data.main.temp)}°C`;
-  })
-  .catch(error => {
-    console.error('Erro:', error);
-  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.lat}&lon=${userLocation.lon}&appid=${APIkey}&units=metric`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => degrees.innerHTML = `${parseInt(data.main.temp)}°C`)
+    .catch(error => console.error(error));
+}
+
+const errorCurrentPosition = (erro) => {
+  console.error(erro);
+
+  userLocation = {
+    lat: -23.5489,
+    lon: -46.6388
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.lat}&lon=${userLocation.lon}&appid=${APIkey}&units=metric`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => degrees.innerHTML = `${parseInt(data.main.temp)}°C`)
+    .catch(error => console.error(error));
+}
+
+navigator.geolocation.getCurrentPosition(currentPosition, errorCurrentPosition)
